d2ed43898d9f14eb01e8eecbd1732b4f39ad04b3
Jason Yao <jasony.edu@gmail.com>
2015-12-17 20:36:36 -0500

    Cleaned up unused code

--- src/InvalidInputException.java
+++ src/InvalidInputException.java
@@ -2,7 +2,4 @@ public class InvalidInputException extends Exception
 {
     public InvalidInputException()
     {super();}
-
-    public InvalidInputException(String message)
-    {super(message);}
 } // End of the Invalid Input exception
